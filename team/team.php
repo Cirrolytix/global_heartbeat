8e2137c3d8ce3e8b42965040dac4b8540e44c95a
cricketsoong <cricket.soong@gmail.com>
2020-10-05 02:23:46 +0800

    team

---
+++ team/team.php
@@ -0,0 +1,41 @@
+                                                         <!-- Team Container -->
+<div class=\"w3-container w3-center w3-padding\" id=\"team\">
+	<div class=\"w3-row\">
+                <div class=\"w3-third  w3-padding-12\">
+                  <img src=\"resources/images/deniseR.jpg\"  style=\"width:30%\" class=\"w3-circle w3-hover-opacity\">
+                  <h3>Denise Brennan-Rieder</h3>
+                  <p>Medical Researcher, Protein Chemistry</p>
+                <a href=\"https://www.linkedin.com/in/thesciencemaster/" class=\"fa fa-linkedin\" style='background: #007bb5; padding: 5px' ></a>
+                </div>
+                
+                <div class=\"w3-third  w3-padding-12\">
+                  <img src=\"resources/images/cricketS.jpg\" style=\"width:30%\" class=\"w3-circle w3-hover-opacity\">
+                  <h3>Cricket Soong</h3>
+                  <p>Emerging Technology Researcher at CirroLytix</p>
+                  <a href=\"https://www.linkedin.com/in/cricketeer\" class=\"fa fa-linkedin\" style='background: #007bb5; padding: 5px' ></a>
+                </div>
+                
+                <div class=\"w3-third  w3-padding-12\">
+                  <img src=\"resources/images/EmsV.jpg\" style=\"width:30%\" class=\"w3-circle w3-hover-opacity\">
+                  <h3>Emily Jo Vizmonte</h3>
+                  <p>Analytics Consultant at CirroLytix</p>
+                  <a href=\"https://www.linkedin.com/in/emily-jo-vizmonte-b7a09380\" class=\"fa fa-linkedin\" style='background: #007bb5; padding: 5px' ></a>
+                </div>
+    </div>
+	
+    <div class=\"w3-row w3-center\"><br>
+                <div class=\"w3-third  w3-padding-12\">
+                  <img src=\"resources/images/racheM.jpg\" style=\"width:30%\" class=\"w3-circle w3-hover-opacity\">
+                  <h3>Rachelle Leah Melendres</h3>
+                  <p>Data Science | Research</p>
+                  <a href=\"https://www.linkedin.com/in/rachemelendres\" class=\"fa fa-linkedin\" style='background: #007bb5; padding: 5px' ></a>
+                </div>
+                
+                <div class=\"w3-third  w3-padding-12\">
+                  <img src=\"resources/images/markP.jpg\" style=\"width:30%\" class=\"w3-circle w3-hover-opacity\">
+                  <h3>Mark Pascual</h3>
+                  <p>Full Stack Developer | Data Science</p>
+                  <a href=\"https://www.linkedin.com/in/markpascual1986\" class=\"fa fa-linkedin\" style='background: #007bb5; padding: 5px' ></a>
+                </div>
+    </div>
+	<br>
